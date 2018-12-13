0e54979fe93e2313adfa50f66d1910e99bda4bf6
weedrice <supp0rtyoo@gmail.com>
2018-12-14 02:18:10 +0900

    UI 구성

--- app/src/main/java/kr/co/supp0rtyoo/boostcourse/MainActivity.java
+++ app/src/main/java/kr/co/supp0rtyoo/boostcourse/MainActivity.java
@@ -2,12 +2,22 @@ package kr.co.supp0rtyoo.boostcourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+    RatingBar movieRatingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        movieRatingBar.setStepSize(0.25f);
+        RatingBar.OnRatingBarChangeListener listener = movieRatingBar.getOnRatingBarChangeListener();
+
+    }
+
+    private void findID() {
+        movieRatingBar = (RatingBar)findViewById(R.id.movieRatingBar);
     }
 }
