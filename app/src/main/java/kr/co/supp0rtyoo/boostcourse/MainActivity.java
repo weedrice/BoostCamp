d0debc07b07da5d1928b814d947412bdf80aa876
weedrice <supp0rtyoo@gmail.com>
2018-12-17 07:47:19 +0900

    Toast 메세지 출력되지 않던 오류 수정 WebView Activity 삭제하고 Chrome Custom Tab 추가 (Close Button 이미지를 구할수 없어 바꾸지 못함) & 애니메이션 추가

--- app/src/main/java/kr/co/supp0rtyoo/boostcourse/MainActivity.java
+++ app/src/main/java/kr/co/supp0rtyoo/boostcourse/MainActivity.java
@@ -1,15 +1,19 @@
 package kr.co.supp0rtyoo.boostcourse;
 
-import android.content.Intent;
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 import kr.co.supp0rtyoo.boostcourse.databinding.ActivityMainBinding;
 import kr.co.supp0rtyoo.boostcourse.movieInfo.MovieInfo;
@@ -69,15 +73,20 @@ public class MainActivity extends AppCompatActivity implements Movie.View{
 
     @Override
     public void startWebActivity(String url) {
-        Intent intent = new Intent(getApplicationContext(), WebActivity.class);
-        intent.putExtra("url", url);
-        startActivity(intent);
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setStartAnimations(MainActivity.this, R.anim.slide_in_right, R.anim.slide_out_left);
+        builder.setExitAnimations(MainActivity.this, R.anim.slide_in_left, R.anim.slide_out_right);
+        builder.setToolbarColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(MainActivity.this, Uri.parse(url));
     }
 
     @Override
     public void showToastMessage(String message) {
         //Toast toast = Toast.makeText(this, "'"+binding.searchText.getText().toString()+"' 검색결과는 없습니다.",Toast.LENGTH_LONG);
         View toastView = View.inflate(MainActivity.this, R.layout.toast_layout, null);
+        TextView textView = (TextView)toastView.findViewById(R.id.toastText);
+        textView.setText(message);
         Toast toast = new Toast(MainActivity.this);
         toast.setView(toastView);
         toast.setDuration(Toast.LENGTH_LONG);
--- app/src/main/java/kr/co/supp0rtyoo/boostcourse/MoviePresenter.java
+++ app/src/main/java/kr/co/supp0rtyoo/boostcourse/MoviePresenter.java
@@ -14,12 +14,17 @@ public class MoviePresenter implements Movie.Presenter {
 
     @Override
     public void setRecyclerView(String text) {
-        if(movieModel.getMovieInfo(text).getItems().size() == 0) {
-            String message = "'" + text + "' 검색결과는 없습니다.";
-            movieView.showToastMessage(message);
-        } else {
-            movieView.startBinding(movieModel.getMovieInfo(text));
-            movieView.setRecyclerListener();
+        if(text.length() == 0) {
+            movieView.showToastMessage("검색어를 입력해주세요.");
+        }
+        else {
+            if (movieModel.getMovieInfo(text).getItems().size() == 0) {
+                String message = "'" + text + "' 검색결과는 없습니다..";
+                movieView.showToastMessage(message);
+            } else {
+                movieView.startBinding(movieModel.getMovieInfo(text));
+                movieView.setRecyclerListener();
+            }
         }
     }
 
